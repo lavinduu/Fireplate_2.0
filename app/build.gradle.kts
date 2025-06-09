bf623b4a8730021d59c3854339718358b614090e
Thaviya2003 <thaviya2003@gmail.com>
2025-06-09 15:46:44 +0530

    8th commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,15 @@ dependencies {
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation ("androidx.compose.material:material-icons-extended")
 
+    implementation (platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.activity:activity-compose:1.8.2")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
